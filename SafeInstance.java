084ef7c2b3a82a6e261c950e9bcdbd0aa0a7d23a
Michael McGuinness <72993e95@opayq.com>
2016-10-15 13:12:00 +0100

    Update SafeInstance.java

--- SafeInstance.java
+++ SafeInstance.java
@@ -1,17 +1,21 @@
 class Car
 {
+  //Class Initialisations
   private String maker;
   private String colour;
   private String bodyType;
+  //Accelerate Function
   private String accelerate(){
     String motion = "Accelerating...";
     return motion;
   }
+  //setCar Function
   public void setCar(String brand, String paint, String style){
     maker = brand;
     colour = paint;
     bodyType = style;
   }
+  //getCar Function
   public void getCar(){
     System.out.println(maker + " paint is " + colour);
     System.out.println(maker + " style is " + bodyType);
@@ -21,10 +25,13 @@ class Car
 class SafeInstance
 {
   public static void main(String args[]){
-    Car Porsche = new Car();
+    //Initialisations
+    Car Porsche = new Car(), Bentley = new Car();
+    //Actions
+    //Porsche
     Porsche.setCar("Porsche","Red","Coupe");
     Porsche.getCar();
-    Car Bentley = new Car();
+    //Bentley
     Bentley.setCar("Bentley","Green","Saloon");
     Bentley.getCar();
   }
