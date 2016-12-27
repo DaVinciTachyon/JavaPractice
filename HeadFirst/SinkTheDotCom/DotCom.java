a50d8952fd8715740504018b7cc29f6fa1b7e709
Michael McGuinness <72993e95@opayq.com>
2016-12-27 17:29:40 +0000

    GameHelper class updated fully

--- HeadFirst/SinkTheDotCom/DotCom.java
+++ HeadFirst/SinkTheDotCom/DotCom.java
@@ -4,8 +4,6 @@ public class DotCom
 {
   //Declare instance variables
   private ArrayList<String> locationCells;
-  private int sizeDotCom;
-  //private int lengthBoard;
   private String name;
 
   //Set the name of the DotCom
@@ -14,31 +12,7 @@ public class DotCom
     name = domainName + ".com";
   }
 
-  //Set the size of the DotCom
-  public void setSizeDotCom(int s)
-  {
-    if(s > 0)
-      sizeDotCom = s;
-    else if(s == 0)
-      sizeDotCom = 1;
-    else
-      sizeDotCom = -1 * s;
-  }
-
-  //Set the size of the DotCom
-  /*public void setLengthBoard(int s)
-  {
-    lengthBoard = s;
-  }*/
-
   //Get the locations of the DotCom
-  //If the size of the DotCom is yet to be determined
-  public void setLocationCells(int s, ArrayList<String> loc)
-  {
-    sizeDotCom = s;
-    setLocationCells(loc);
-  }
-  //If the size of the DotCom has already been decided
   public void setLocationCells(ArrayList<String> loc)
   {
     locationCells = loc;
--- HeadFirst/SinkTheDotCom/GameHelper.java
+++ HeadFirst/SinkTheDotCom/GameHelper.java
@@ -1,7 +1,14 @@
 import java.io.*;
+import java.util.*;
 public class GameHelper
 //Create a class that allows inputs to be taken
 {
+  private static final String alphabet = "abcdefg";
+  private int gridLength = 7;
+  private int gridSize = 49;
+  private int[] grid = new int[gridSize];
+  private int comCount = 0;
+
   public String getUserInput(String prompt)//gets user's input
   {
     //Declarations
@@ -26,6 +33,56 @@ public class GameHelper
       System.out.println("IOException: " + e);
     }
     //Return the input
-    return inputLine;
+    return inputLine.toLowerCase();
+  }
+
+  public ArrayList<String> placeDotCom(int comSize)
+  {
+    ArrayList<String> alphaCells = new ArrayList<String>();
+    String temp = null;
+    int[] coords = new int[comSize];
+    int attempts = 0;
+    boolean success = false;
+    int location = 0;
+
+    comCount++;
+    int incr = 1;
+    if(comCount % 2 == 1)
+      incr = gridLength;
+
+    while(!success & attempts++ < 200)
+    {
+      location = (int) (Math.random() * gridSize);
+      int x = 0;
+      success = true;
+      while(success && x < comSize)
+      {
+        if(grid[location] == 0)
+        {
+          coords[x++] = location;
+          location += incr;
+          if(location >= gridSize)
+            success = false;
+          if(x > 0 && (location % gridLength == 0))
+            success = false;
+          else
+            success = false;
+        }
+      }
+      int x = 0;
+      int row = 0;
+      int column = 0;
+      while(x < comSize)
+      {
+        grid[coords[x]] = 1;
+        row = (int) (coords[x] / gridLength);
+        column = coords[x] % gridLength;
+        temp = String.valueOf(alphabet.charAt(column));
+        alphaCells.add(temp.comcat(Integer.toString(row)));
+        x++;
+        //System.out.print("Coord " + x + " = " + alphaCells.get(x-1));
+      }
+      return alphaCells;
+    }
   }
 }
